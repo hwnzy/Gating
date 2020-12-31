31e8f315bbf756e7bc5db61524bab3cc80cd3f1f
hwnzy <hwnzy@foxmail.com>
2020-12-31 00:55:28 -0800

    Optimize http.Handler interface.

--- main.go
+++ main.go
@@ -6,20 +6,23 @@ import (
 	"net/http"
 )
 
-func main(){
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":8001", nil))
-}
+// Engine is the uni handler for all requests
+type Engine struct{}
 
-// handler echoes r.URL.Path
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-}
-
-// handler echoes r.URL.Path
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request){
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
 	}
+}
+
+func main(){
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":8001", engine))
 }
