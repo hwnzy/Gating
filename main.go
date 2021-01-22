7b81747983489b93188860a8dc85d7274b2da553
hwnzy <hwnzy@foxmail.com>
2021-01-22 12:33:46 -0800

    Add RouterGroup.

--- main.go
+++ main.go
@@ -8,20 +8,32 @@ import (
 
 func main() {
 	r := gating.New()
-	r.GET("/", func(c *gating.Context){
-		c.HTML(http.StatusOK, "<h1>Hello Gating</h1>")
+	r.GET("/index", func(c *gating.Context){
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
-	r.GET("/hello", func(c *gating.Context){
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "Hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-
-	r.POST("/login", func(c *gating.Context){
-		c.JSON(http.StatusOK, gating.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gating.Context){
+			c.HTML(http.StatusOK, "<h1>Hello, Gating</h1>")
 		})
-	})
+		v1.GET("/hello", func(c *gating.Context){
+			// expect /hello?name=hwnzy
+			c.String(http.StatusOK, "Hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gating.Context){
+			// expect /hello/hwnzy
+			c.String(http.StatusOK, "Hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gating.Context){
+			c.JSON(http.StatusOK, gating.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 
 	r.Run(":8001")
 }
