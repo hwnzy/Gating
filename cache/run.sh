39a12b71eec8c52896d9769aed21c03ee97684e5
hwnzy <hwnzy@foxmail.com>
2021-02-05 16:25:42 -0800

    Add more test.

--- cache/run.sh
+++ cache/run.sh
@@ -11,5 +11,10 @@ echo ">>> start test"
 curl "http://localhost:9999/api?key=Tom" &
 curl "http://localhost:9999/api?key=Tom" &
 curl "http://localhost:9999/api?key=Tom" &
-
+curl "http://localhost:9999/api?key=Tom" &
+curl "http://localhost:9999/api?key=Tom" &
+curl "http://localhost:9999/api?key=Tom" &
+curl "http://localhost:9999/api?key=Tom" &
+curl "http://localhost:9999/api?key=Tom" &
+curl "http://localhost:9999/api?key=Tom" &
 wait
